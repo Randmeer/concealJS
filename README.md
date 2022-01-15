90dc9e18015124785597ba68869d818f5de7f472
Randmeer <das.randmeer@gmail.com>
2022-01-15 01:19:04 +0100

    custom html tags, fullscreen, documentation

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # uncover.js
-Create Presentations on the Web (totally original name btw)
+Minimalistic, quick and fast Presentations on the Web.
+
+## Documentation
+
+### \<slide\>
+
+In uncover.js, the html body consists out of slides.
+They always have the dimensions of the user's viewport and can be navigated using the Up and Down Arrow Keys.
+This way, the presentation is always in the correct aspect ratio. You may have to zoom in/out in the browser to
+get optimal results.
+
+
+### \<p\>
+
+The paragraph `<p>` is one of the few elements that has no inline display. Therefore it is used to include all the inline-elements
+like `<bt>`, `<a>`, etc. Of course all the text goes there too. Use `<br>` to break lines instead of creating multiple
+paragraphs.
+
+
+### \<h1\> - \<h6\>
+
+The headers `<h1>` to `<h3>` are inline elements and can be used for titles, subtitles or just to highlight words in a paragraph.
+
+
+### \<bt\>
+
+The button `<bt>` is an inline-element and, well, a button.
+
+
+### \<row\> and \<col\>
+
+The containers `<row>` and `<col>` are Shortcuts for flex-boxes with the `row` or `column` direction.
+
+
+### \<hs\> and \<vs\>
+
+The horizontal space `<hs>` and the vertical space `<vs>` are used to separate elements. Their innerHTML is the width/height in pixels.
+
+
+
+
+
--- uncover.js
+++ uncover.js
@@ -1,70 +1,146 @@
 var pageindex = 0;
+var fullscreen = false;
 
 // disable scrollbar
 document.body.style.overflow = 'hidden';
 
-// remove arrow key scrolling and
-window.addEventListener("keydown", function(e) {
-    if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+// remove arrow key scrolling and add shortcuts
+window.addEventListener("keydown", function (e) {
+    if (["Space", "ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].indexOf(e.code) > -1) {
         e.preventDefault();
     }
-    if(e.code === "ArrowUp") {
+    if (e.code === "ArrowUp") {
         pageindex--;
         try {
             let page = document.body.children[pageindex];
-            if (page.className === "page") {
+            if (page.tagName === "SLIDE") {
                 page.scrollIntoView();
-            } else pageindex ++;
-        } catch {pageindex ++;}
+            } else pageindex++;
+        } catch {
+            pageindex++;
+        }
     } else if (e.code === "ArrowDown") {
         pageindex++;
         try {
             let page = document.body.children[pageindex];
-            if (page.className === "page") {
+                if (page.tagName === "SLIDE") {
                 page.scrollIntoView();
-            } else pageindex --;
-        } catch {pageindex --;}
+            } else pageindex--;
+        } catch {
+            pageindex--;
+        }
+    } else if (e.code === "KeyF") {
+        if (fullscreen) closeFullscreen();
+        else openFullscreen();
+        fullscreen = !fullscreen;
     }
+
 }, false);
 
-// add page stylesheet
-var link = document.createElement('style');
-link.type = 'text/css';
-link.innerHTML = `
+var head = document.getElementsByTagName('HEAD')[0];
+
+// add styles
+var css = document.createElement('style');
+css.type = 'text/css';
+css.innerHTML = `
 html {
     scroll-behavior: smooth;
 }
 body {
     background-color: #fefaef;
+    color: #646669;
+    font-family: 'Roboto Mono';
 }
-.page {
+slide {
+    display: block;
     height: 100vh;
     box-sizing: border-box; /* include padding in box height*/
-    /*border: 4px solid #646669;*/
     padding: 50px;
 }
-.button {
+row {
+    display: flex;
+    flex-direction: row;
+}
+column {
+    display: flex;
+    flex-direction: column;
+}
+bt {
+    display: inline;
     -webkit-touch-callout: none;
     -webkit-user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
     user-select: none;
-    cursor:pointer;
+    cursor: pointer;
     padding: 10px;
     background-color: #646669;
     border-radius: 8px;
-    font-family: robotoMonoMedium, monospace;
+    border: none;
     color: #fefaef;
+    font-size: 25px;
+    font-family: 'Roboto Mono';
 }
-text {
+p {
     color: #646669;
     font-size: 25px;
-    font-family: robotoMonoMedium, monospace;
 }
-header {
+bt a {
+  color: #fefaef;
+  text-decoration: none;
+}
+h1 {
+    display: inline;
     color: #646669;
-    font-size: 40px;
-    font-family: robotoMonoMedium, monospace;
+    font-size: 50px;
+    font-weight: bold;
+}
+hs {
+    display: block;
+    height: 50px;
+}
+vs {
+    display: block;
+    width: 50px;
 }
 `
-document.getElementsByTagName('HEAD')[0].appendChild(link);
+head.appendChild(css);
+
+// add font
+var font = document.createElement('link');
+font.href= "https://fonts.googleapis.com/css?family=Roboto Mono";
+font.rel = "stylesheet";
+head.appendChild(font);
+
+// process hs & vs
+var hspaces = document.getElementsByTagName('hs');
+for (var i=0; i<hspaces.length; i++) {
+    hspaces[i].style.width = hspaces[i].innerHTML + "px";
+    hspaces[i].innerHTML = "";
+}
+var vspaces = document.getElementsByTagName('vs');
+for (var i=0; i<vspaces.length; i++) {
+    vspaces[i].style.height = vspaces[i].innerHTML + "px";
+    vspaces[i].innerHTML = "";
+}
+
+// fullscreen functions
+function openFullscreen() {
+    elem = document.documentElement;
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
+}
